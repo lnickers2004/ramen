95bce8d55081909b9ddd0df5e6128242b00c032a
Masaya Ando <mando@bu.edu>
2014-06-10 13:08:08 -0400

    web compatible

--- ramen_test.js
+++ ramen_test.js
@@ -130,16 +130,3 @@ describe("Ramen", function() {
 		});
 	});
 })
-
-
-var express = require("express");
-var app = express();
-var port = Number(process.env.PORT || 8080);
-
-app.get('/', function(req, res) { 
-	res.sendfile('./web/ramen.html');
-});
-
-app.listen(port, function() {
-	console.log("Listening on " + port);
-});
